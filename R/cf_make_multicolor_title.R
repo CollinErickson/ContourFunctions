819ce5bfce102a032c56405182c8a3526abc4839
Collin Erickson <collinberickson@gmail.com>
2017-04-19 09:58:19 -0500

    Added cex.main option

--- R/cf_make_multicolor_title.R
+++ R/cf_make_multicolor_title.R
@@ -1,4 +1,4 @@
-make.multicolor.title <- function(main, z, pretitle, posttitle, mainminmax_minmax) {
+make.multicolor.title <- function(main, z, pretitle, posttitle, mainminmax_minmax, cex.main=par()$cex.main) {
   if(is.null(main)) {
     title_text <- c(pretitle)
     title_color <- c(1)
@@ -8,8 +8,8 @@ make.multicolor.title <- function(main, z, pretitle, posttitle, mainminmax_minma
     }
     title_text  <- c(title_text, "[",signif(min(z),3),', ',signif(max(z),3),']',posttitle)
     title_color <- c(title_color,1,  1,               1,   1,               1,  1)
-    multicolor.title(title_text,title_color)
+    multicolor.title(title_text,title_color, cex.main=cex.main)
   } else {
-    multicolor.title(main, 1)
+    multicolor.title(main, 1, cex.main=cex.main)
   }
 }
--- R/cf_multicolor_title.R
+++ R/cf_multicolor_title.R
@@ -2,27 +2,28 @@
 #' @param main  Text to put in main title of plot
 #' @param col.main  Colors to use for the text
 #' @param collapse  What to put between elements of main, defaults to "" but " " might be appropriate
+#' @param cex.main The size of the main title. 1.2 is default.
 #' @examples 
 #' plot(1:4)
 #' multicolor.title(c('Black, ','red, ','green'),c(1,2,3))
 #' @export
-multicolor.title <- function(main,col.main,collapse='') {
+multicolor.title <- function(main,col.main, collapse='', cex.main=par()$cex.main) {
   if (length(main) != length(col.main)) {stop('main and col must have same length')}
   n <- length(main)
   if(n==1) {
-    title(bquote(.(main[1])),col.main=col.main[1])
+    title(bquote(.(main[1])),col.main=col.main[1], cex.main=cex.main)
   } else {
     # print first
-    title(bquote(.(main[1]) * phantom(.(paste0(main[2:n],collapse=collapse)))),col.main=col.main[1])
+    title(bquote(.(main[1]) * phantom(.(paste0(main[2:n],collapse=collapse)))),col.main=col.main[1], cex.main=cex.main)
     
     # print middle
     if(n > 2) {
       for(i in 2:(n-1)) {
-        title(bquote(phantom(.(paste0(main[1:(i-1)],collapse=collapse))) * .(main[i]) * phantom(.(paste0(main[(i+1):n],collapse=collapse)))),col.main=col.main[i]) 
+        title(bquote(phantom(.(paste0(main[1:(i-1)],collapse=collapse))) * .(main[i]) * phantom(.(paste0(main[(i+1):n],collapse=collapse)))),col.main=col.main[i], cex.main=cex.main) 
       }
     }
     
     # print last
-    title(bquote(phantom(.(paste0(main[1:(n-1)],collapse=collapse))) * .(main[n])),col.main=col.main[n])
+    title(bquote(phantom(.(paste0(main[1:(n-1)],collapse=collapse))) * .(main[n])),col.main=col.main[n], cex.main=cex.main)
   }
 }
