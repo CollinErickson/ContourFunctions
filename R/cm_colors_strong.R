4f21bf191f8d12f4fe8302043a702b129664ee39
Collin Erickson <collinberickson@gmail.com>
2019-05-11 19:05:01 -0500

    gcf_grid uses ggplot2 to replicate cf_grid

--- R/cm_colors_strong.R
+++ R/cm_colors_strong.R
@@ -26,9 +26,9 @@ cm.colors.strong <- function (n, alpha = 1) {
     k <- n%/%2L
     l1 <- k + 1L - even.n
     l2 <- n - k + even.n
-    c(if (l1 > 0L) hsv(h = 6/12, s = seq.int(0.99, ifelse(even.n, 
+    c(if (l1 > 0L) grDevices::hsv(h = 6/12, s = seq.int(0.99, ifelse(even.n, 
                                                          0.99/k, 0), length.out = l1), v = 1, alpha = alpha), 
-      if (l2 > 1) hsv(h = 10/12, s = seq.int(0, 0.99, length.out = l2)[-1L], 
+      if (l2 > 1) grDevices::hsv(h = 10/12, s = seq.int(0, 0.99, length.out = l2)[-1L], 
                       v = 1, alpha = alpha))
   }
   else character()
---
+++ R/gcf_grid.R
@@ -0,0 +1,149 @@
+#' Create contour plot from grid data using ggplot2
+#' 
+#' The same as cf_grid_screen but uses ggplot2 for the plot.
+#'
+#' @param x  x values, must form grid with y.
+#' If not given, it is assumed to be from 0 to 1.
+#' @param y  y values, must form grid with x.
+#' If not given, it is assumed to be from 0 to 1.
+#' @param z  z values at grid locations
+#' @param xlim  x limits for the plot.
+#' @param ylim  y limits for the plot.
+#' @param zlim  z limits for the plot.
+#' @param levels  a set of levels which are used to partition the range of z.
+#' Must be strictly increasing (and finite). Areas with z values between
+#' consecutive levels are painted with the same color.
+#' @param nlevels  if levels is not specified, the range of z, values is
+#' divided into approximately this many levels.
+#' @param color.palette  a color palette function to be used to assign colors
+#' in the plot. Defaults to cm.colors. Other options include rainbow,
+#' heat.colors, terrain.colors, topo.colors, and function(x) {gray((1:x)/x)}.
+#' @param col  an explicit set of colors to be used in the plot. This argument
+#' overrides any palette function specification. There should be one less
+#' color than levels
+#' @param asp  the y/x aspect ratio, see plot.window.
+#' @param las  the style of labeling to be used. The default is to use
+#' horizontal labeling.
+#' @param bar Should a bar showing the output range and colors be shown on the right?
+#' @param pts Points to plot on top of contour
+#' @param reset.par Should the graphical parameters be reset before exiting?
+#' Usually should be unless you need to add something to the plot
+#' afterwards and bar is TRUE.
+#' @param pretitle Text to be preappended to end of plot title
+#' @param posttitle Text to be appended to end of plot title
+#' @param main Title for the plot
+#' @param mainminmax  whether the min and max values should be shown
+#' in the title of plot
+#' @param mainminmax_minmax Whether [min,max]= should be shown in title
+#' or just the numbers
+#' @param afterplotfunc Function to call after plotting, such as adding
+#' points or lines.
+#' @param cex.main The size of the main title. 1.2 is default.
+#' @param ...  additional graphical parameters, currently only passed to title().
+#' 
+#' @return ggplot2 object
+#' @export
+#'
+#' @examples
+#' x <- y <- seq(-4*pi, 4*pi, len = 27)
+#' r <- sqrt(outer(x^2, y^2, "+"))
+#' gcf_grid(cos(r^2)*exp(-r/(2*pi)))
+#' gcf_grid(r, color.palette=heat.colors, bar=TRUE)
+#' gcf_grid(r, color.palette=function(x) {gray((1:x)/x)}, bar=TRUE)
+gcf_grid <-  function (x = seq(0, 1, length.out = nrow(z)), 
+                       y = seq(0, 1,length.out = ncol(z)), z,
+                       xlim = range(x, finite = TRUE),
+                       ylim = range(y, finite = TRUE),
+                       zlim = range(z, finite = TRUE),
+                       levels = pretty(zlim, nlevels), nlevels = 20,
+                       color.palette = cm.colors.strong,
+                       col = color.palette(length(levels) - 1),
+                       asp = NA, las = 1,
+                       bar=F, pts=NULL, reset.par=TRUE,
+                       pretitle="", posttitle="", main=NULL,
+                       mainminmax=!bar, mainminmax_minmax=TRUE,
+                       afterplotfunc=NULL,
+                       cex.main=par()$cex.main,
+                       ...) {#browser()
+  if (missing(z)) {
+    if (!missing(x)) {
+      if (is.list(x)) {
+        z <- x$z
+        y <- x$y
+        x <- x$x
+      }
+      else {
+        z <- x
+        x <- seq.int(0, 1, length.out = nrow(z))
+      }
+    }
+    else stop("no 'z' matrix specified")
+  }
+  else if (is.list(x)) {
+    y <- x$y
+    x <- x$x
+  }
+  if (any(diff(x) <= 0) || any(diff(y) <= 0))
+    stop("increasing 'x' and 'y' values expected")
+  
+  if (any(diff(x) <= 0) || any(diff(y) <= 0)) 
+    stop("increasing 'x' and 'y' values expected")
+  
+  
+  t2 <- cbind(expand.grid(x, y), tz=c(z))
+  p <- ggplot2::ggplot() + 
+    ggplot2::geom_raster(ggplot2::aes_string(x="Var1", y="Var2", fill = "tz"),
+                                                t2, interpolate=TRUE)+
+    # scale_fill_gradientn(colours=c("cyan","white","magenta"))
+    ggplot2::scale_fill_gradientn(colours=col)
+  
+  if (bar) {
+    # Remove title on legend
+    p <- p + ggplot2::theme(legend.title = ggplot2::element_blank())
+  }
+  if (!bar) {
+    p <- p + ggplot2::theme(legend.position = "none") 
+  }
+  
+  # Remove axes labels
+  p <- p + ggplot2::theme(axis.title.x = ggplot2::element_blank()) + 
+    ggplot2::theme(axis.title.y = ggplot2::element_blank())
+  
+  # Cut off extra space
+  p <- p + ggplot2::coord_cartesian(xlim=c(min(x), max(x)), ylim=c(min(y), max(y)), expand=F)
+  
+  
+  if (mainminmax | !is.null(main)) {
+    # Unable to put colors in title
+    # p <- p + gg_make_multicolor_title()
+    p <- p + ggplot2::ggtitle(paste0("[min,max]=[",signif(min(z),3),",", signif(max(z),3),"]")) + 
+      ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))
+    # Max/min annotations since colors don't work
+    minind <- arrayInd(which.min(z), dim(z))
+    p <- p + ggplot2::annotate("text", x = x[minind[1]], y = y[minind[1]], label = signif(min(z),4))
+    maxind <- arrayInd(which.max(z), dim(z))
+    p <- p + ggplot2::annotate("text", x = x[maxind[2]], y = y[maxind[2]], label = signif(max(z),4))
+  }
+  
+  if (!is.null(pts)) {
+    if (!is.matrix(pts)) { # if not a matrix, make it a matrix by row
+      if (is.numeric(pts) && (length(pts)%%2==0)) {
+        pts <- matrix(pts, ncol=2, byrow = T)
+      }
+    }
+    # points(pts, pch=19)
+    p <- p + ggplot2::geom_point(ggplot2::aes_string("V1","V2"), data=as.data.frame(pts))
+  }
+  
+  if (!is.null(afterplotfunc)) {
+    afterplotfunc()
+  }
+  
+  if (reset.par) {# Either reset parameters
+  } else { # or return it so user can do it later
+    stop("Can't reset later, this returns the plot")
+    # return(reset.par.func)
+  }
+  p
+}
+
